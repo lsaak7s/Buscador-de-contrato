ff3ee82e790495feaa5e4a8035c7963ad42f4979
Isaac Alves <isaacsantos.7r@gmail.com>
2026-07-24 15:05:20 -0300

    Andamento

---
+++ Buscador de contatos/script.js
@@ -0,0 +1,32 @@
+const resultado = document.getElementById("res");
+const input = document.getElementById("inp");
+const buttom = document.getElementById("bto");
+buttom.addEventListener("click", buscar);
+
+
+
+
+
+function buscar() {
+    const contatos = {
+        nome = lucio, numero
+    }
+
+    if (input == contatos) {
+        console.log(contatos)
+    }
+
+
+    else {
+        console.log("Não achado")
+    }
+}
+
+/*
+function buscar() {
+  for (let i = 0; i < contatos.length; i++) {
+    const element = contatos[i];
+    console.log(element);
+  }
+}
+*/
