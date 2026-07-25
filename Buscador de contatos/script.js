5be2dca95f085c07f8ed478d8ab2231d521a018e
Isaac Alves <isaacsantos.7r@gmail.com>
2026-07-24 22:48:46 -0300

    Quase lá

--- Buscador de contatos/script.js
+++ Buscador de contatos/script.js
@@ -1,32 +1,46 @@
 const resultado = document.getElementById("res");
+const button = document.getElementById("bto");
 const input = document.getElementById("inp");
-const buttom = document.getElementById("bto");
-buttom.addEventListener("click", buscar);
 
+button.addEventListener("click", buscar);
 
-
-
-
+const contatos = [
+    {
+        nome: "AMANDA",
+        telefone: "4586318885",
+    },
+    {
+        nome: "BRUNA",
+        telefone: "4586318485",
+    },
+    {
+        nome: "CARLA",
+        telefone: "4586318785",
+    },
+    {
+        nome: "DIANA",
+        telefone: "4586318185",
+    },
+];
 function buscar() {
-    const contatos = {
-        nome = lucio, numero
+    let encontrou = false;
+
+    const valordoinput = input.value;
+
+    for (let i = 0; i < contatos.length; i++) {
+        const element = contatos[i];
+
+        if (element.toUpperCase() === valordoinput.toUpperCase()) {
+            resultado.innerHTML =
+                "Nome: " + element.nome + "<br>" +
+                "Telefone: " + element.telefone;
+
+            encontrou = true;
+            break;
+        }
     }
 
-    if (input == contatos) {
-        console.log(contatos)
-    }
-
-
-    else {
-        console.log("Não achado")
+    if (!encontrou) {
+        resultado.innerHTML = "Contato não encontrado";
     }
 }
-
-/*
-function buscar() {
-  for (let i = 0; i < contatos.length; i++) {
-    const element = contatos[i];
-    console.log(element);
-  }
-}
-*/
