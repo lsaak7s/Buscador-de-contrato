b9ca609bc5e9f41ea040439176168d7cd7dd69b5
Isaac Alves <isaacsantos.7r@gmail.com>
2026-07-28 21:26:42 -0300

    s

--- Buscador de contatos/script.js
+++ Buscador de contatos/script.js
@@ -100,7 +100,36 @@ const contatos = [
         nome: "BEATRIZ",
         telefone: "4586318120",
     },
-];
+]; function buscar() {
+    //O interupitor
+    let encontrou = false;
+    // e nessesario para pode ele pode usar o valor
+    const valordoinput = input.value;
+    let i = 0
+    do {
+
+    } while (i < contatos.length) {
+        const element = contatos[i];
+        i++
+        //aqui ele ja esta verificando e deixando em caixa alta
+        if (element.nome.toUpperCase() === valordoinput.toUpperCase()) {
+
+            resultado.innerHTML =
+                "Nome: " + element.nome + "<br>" +
+                "Telefone: " + element.telefone;
+            // sinalizou para o interrupitor que deu certo
+            encontrou = true;
+            // Se achou ele pausa
+            break;
+        }
+
+
+    }
+    //verifica !SE! teve mudança
+    if (!encontrou) {
+        resultado.innerHTML = "Contato não encontrado";
+    }
+}
 /*
 function buscar() {
     //O interupitor
